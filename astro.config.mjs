0b68e283dd285f0f73c15b42256258901c270108
MarceloArraes <marcelo.arraes@gmail.com>
2025-10-16 22:21:42 +1030

    feat: integrating astro with Sanity and insereting markdown on sanity studio to be used here. adding markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,18 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import sanity from '@sanity/astro';
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://example.com',
+    integrations: [mdx(), sitemap(), sanity({
+         projectId: '4hfzjbz1',
+      dataset: 'production',
+      // Set useCdn to false if you're building statically.
+      useCdn: false,
+    //   apiVersion: '2025-16-10'
+    }), react()],
+    
 });
